df97ab8cc96a78da3e4b86c21964d6b0ea2ed9ae
Kazuki Watanabe <k.watanabe@jxpress.net>
2021-02-18 13:39:15 +0900

    ビルドできるように

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 
-```bash
+# Arts
+
+ブラウザの試験環境
+
+```shell
+
+# macos環境の中にgdkが必要
+brew install gdk+3
+
 # 実行
 cargo run
 ```
--- src/parse/html.rs
+++ src/parse/html.rs
@@ -1,8 +1,8 @@
-use std::rc::Rc;
-use html5ever::{parse_document};
 use html5ever::driver::ParseOpts;
-use markup5ever_rcdom::{RcDom, Node};
+use html5ever::parse_document;
 use html5ever::tendril::stream::TendrilSink;
+use markup5ever_rcdom::{Node, RcDom};
+use std::rc::Rc;
 
 pub fn parse(html: &str) -> Rc<Node> {
     let parser = parse_document(RcDom::default(), ParseOpts::default());
@@ -10,11 +10,11 @@ pub fn parse(html: &str) -> Rc<Node> {
     println!("{:?}", dom.document);
 
     /*
-        Node { data: Document, children: RefCell { value: [Node { data: Element { name: QualName { prefix: None, ns: Atom('http://www.w3.org/1999/xhtml' type=static), 
-        local: Atom('html' type=static) }, attrs: RefCell { value: [] }, template_contents: None, mathml_annotation_xml_integration_point: false }, 
-        children: RefCell { value: [Node { data: Element { name: QualName { prefix: None, ns: Atom('http://www.w3.org/1999/xhtml' type=static), 
-        local: Atom('head' type=static) }, attrs: RefCell { value: [] }, template_contents: None, mathml_annotation_xml_integration_point: false }, children: RefCell { value: [] } }, 
-        Node { data: Element { name: QualName { prefix: None, ns: Atom('http://www.w3.org/1999/xhtml' type=static), local: Atom('body' type=static) }, attrs: RefCell { value: [] }, 
+        Node { data: Document, children: RefCell { value: [Node { data: Element { name: QualName { prefix: None, ns: Atom('http://www.w3.org/1999/xhtml' type=static),
+        local: Atom('html' type=static) }, attrs: RefCell { value: [] }, template_contents: None, mathml_annotation_xml_integration_point: false },
+        children: RefCell { value: [Node { data: Element { name: QualName { prefix: None, ns: Atom('http://www.w3.org/1999/xhtml' type=static),
+        local: Atom('head' type=static) }, attrs: RefCell { value: [] }, template_contents: None, mathml_annotation_xml_integration_point: false }, children: RefCell { value: [] } },
+        Node { data: Element { name: QualName { prefix: None, ns: Atom('http://www.w3.org/1999/xhtml' type=static), local: Atom('body' type=static) }, attrs: RefCell { value: [] },
         template_contents: None, mathml_annotation_xml_integration_point: false }, children: RefCell { value: [Node { data: Text { contents: RefCell { value: Tendril<UTF8>(shared: "これはhtmlです") } }, children: RefCell { value: [] } }] } }] } }] } }
     */
 
