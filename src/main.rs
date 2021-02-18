5cd4fd8e2e8ee322c7abf41e85f3ac35a3032641
Kazuki Watanabe <k.watanabe@jxpress.net>
2021-02-18 13:56:54 +0900

    gtkアプリケーションが立ち上がるように

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ extern crate arts;
 
 use arts::parse::html;
 use arts::render::render::walk;
+use arts::window;
 
 fn main() {
-    let node = html::parse("<html>これはhtmlです</html>");
-    walk(node);
+    // let node = html::parse("<html>これはhtmlです</html>");
+    // walk(node);
+    window::init();
 }
--- src/window.rs
+++ src/window.rs
@@ -1,9 +1,24 @@
-extern crate gtk;
 extern crate gio;
+extern crate gtk;
 
-use gtk::prelude::*;
 use gio::prelude::*;
+use gtk::prelude::*;
+
+use gtk::{Application, ApplicationWindow, Button};
 
 pub fn init() {
-
+    let application = Application::new(Some("com.github.arts.example"), Default::default())
+        .expect("failed to initialize GTK Application");
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("First Arts Program.");
+        window.set_default_size(350, 70);
+        let button = Button::with_label("Click me");
+        button.connect_clicked(|button| {
+            println!("Clicked");
+        });
+        window.add(&button);
+        window.show_all();
+    });
+    application.run(&[]);
 }
